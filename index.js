69c56c646093a440bd2928f453c7cdd9408f6437
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-07-05 22:22:13 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+  }
+//writeCards(names);
+
+function countDown(count){
+    let c = 10;
+    while (c >= 0) {
+    console.log(c--);
+}   
+    return count;
+}
+countDown(count);
